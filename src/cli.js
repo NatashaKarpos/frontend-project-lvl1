7d9e0a3759d086e5deaf7d2f4b872d6c1d23f638
Natalia Karpova <natalakarpova@Natalas-MacBook-Air.local>
2022-04-05 19:06:02 +0300

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const askUserName = () => readlineSync.question('May I have your name? ');
+const askUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export default askUserName;
